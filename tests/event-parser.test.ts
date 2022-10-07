07c0607d664b61e97f23a136babe83023f22aa4e
Hiroto Shioi <shioihigg@gmail.com>
2022-10-07 13:56:21 +0900

    Add more test

--- tests/event-parser.test.ts
+++ tests/event-parser.test.ts
@@ -1,5 +1,8 @@
 import { ethers } from "ethers";
-import { parseEventAndGroup, parseEventsAndGroup } from "../src/Dog/event-parser";
+import {
+  parseEventAndGroup,
+  parseEventsAndGroup,
+} from "../src/Dog/event-parser";
 
 describe("event-parser", () => {
   // Frobメッセージをパースできる
@@ -17,6 +20,7 @@ describe("event-parser", () => {
       );
     }
   });
+  // Forkメッセージをパースできる
   test("Should parse fork message", () => {
     const FORK_SAMPLE =
       "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e0870c616d4641552d410000000000000000000000000000000000000000000000000000000000000000000000000000004339dff1bd4f12b89127779969fa92fd5008345700000000000000000000000089b78cfa322f6c5de0abceecab66aee45393cc5a0000000000000000000000004339dff1bd4f12b89127779969fa92fd500834570000000000000000000000000000000000000000000001e7e4171bf4d3a00000000000000000000000000000000000000000000000002cdf8b6115c5bca65f9100000000000000000000000000000000000000000000000000000000";
@@ -31,6 +35,7 @@ describe("event-parser", () => {
       expect(a2).toBe("0x89b78cfa322f6c5de0abceecab66aee45393cc5a");
     }
   });
+  // 同じメッセージの場合、1つに絞る
   test("Should return only one", () => {
     const FORK_SAMPLE =
       "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e0870c616d4641552d410000000000000000000000000000000000000000000000000000000000000000000000000000004339dff1bd4f12b89127779969fa92fd5008345700000000000000000000000089b78cfa322f6c5de0abceecab66aee45393cc5a0000000000000000000000004339dff1bd4f12b89127779969fa92fd500834570000000000000000000000000000000000000000000001e7e4171bf4d3a00000000000000000000000000000000000000000000000002cdf8b6115c5bca65f9100000000000000000000000000000000000000000000000000000000";
@@ -40,45 +45,74 @@ describe("event-parser", () => {
   test("Should parse multiple messages", () => {
     let msgs: string[] = [];
     for (let i = 0; i < 10; i++) {
-        msgs.push(createMessage());
+      msgs.push(createMessage());
     }
     const result = parseEventsAndGroup(msgs);
     expect(result.size).toBe(10);
-  })
+  });
+  // ilkが同じ場合にはグループする
+  test("Should group into ilks", () => {
+    let msgs: string[] = [];
+    const ilk = createIlk();
+    for (let i = 0; i < 10; i++) {
+      msgs.push(createMessage({ ilk: ilk }));
+    }
+    const result = parseEventsAndGroup(msgs);
+    expect(result.size).toBe(1);
+    const addresses = Array.from(result.values()).reduce((prev, curr) => {
+        return prev.concat(Array.from(curr));
+    }, [] as string[])
+    expect(addresses.length).toBe(10)
+  });
+  // アドレスが同じでもilkが異なる場合には別々にパースするÏ
+  test("Should record if ilk is different", () => {
+    let msgs: string[] = [];
+    const addr = createAddress();
+    for (let i = 0; i < 10; i++) {
+      msgs.push(createMessage({ address1: addr }));
+    }
+    const result = parseEventsAndGroup(msgs);
+    expect(result.size).toBe(10);
+  });
 });
 
 describe("createAddress()", () => {
-    test("isAddress", () => {
-        const result = ethers.utils.isAddress(toAddress(createAddress()));
-        expect(result).toBe(true);
-    })
-})
+  test("isAddress", () => {
+    const result = ethers.utils.isAddress(toAddress(createAddress()));
+    expect(result).toBe(true);
+  });
+});
 
 function toHex(data: string): string {
-    return `0x${data}`;
-  }
-  
+  return `0x${data}`;
+}
+
 function toAddress(data: string): string {
-    return toHex(data.slice(24));
-  }
+  return toHex(data.slice(24));
+}
 
 function createIlk(): string {
-    const IlkLength = 32;
-    const rand = ethers.utils.randomBytes(IlkLength);
-    return Buffer.from(rand).toString("hex");
+  const IlkLength = 32;
+  const rand = ethers.utils.randomBytes(IlkLength);
+  return Buffer.from(rand).toString("hex");
 }
 
 function createAddress(): string {
-    const ADDR_LENGTH = 20;
-    const rand = ethers.utils.randomBytes(ADDR_LENGTH);
-    return `${"0".repeat(24)}${Buffer.from(rand).toString('hex')}`
+  const ADDR_LENGTH = 20;
+  const rand = ethers.utils.randomBytes(ADDR_LENGTH);
+  return `${"0".repeat(24)}${Buffer.from(rand).toString("hex")}`;
 }
 
-function createMessage(someIlk?: string) {
-    const prefix = "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e0"
-    const event = "76088703";
-    const ilk = someIlk || createIlk();
-    const addr1 = createAddress();
-    const addr2 = createAddress();
-    return `${prefix}${event}${ilk}${addr1}${addr2}`
+function createMessage(args?: {
+  ilk?: string;
+  address1?: string;
+  address2?: string;
+}) {
+  const prefix =
+    "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e0";
+  const event = "76088703";
+  const ilk = args?.ilk || createIlk();
+  const addr1 = args?.address1 || createAddress();
+  const addr2 = args?.address2 || createAddress();
+  return `${prefix}${event}${ilk}${addr1}${addr2}`;
 }
